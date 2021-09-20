19857589e4f15bef843e9b4706302598336e60be
Dan <danielkbeale@gmail.com>
2021-09-20 23:48:00 +0100

    sortCat + sortUser completed

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,1 +1,14 @@
 // extract any functions you are using to manipulate your data, into this file
+//create functions that take an array of objects, take each objects values and puts them in an array of arrays
+
+exports.sortCategories = (catagories) => {
+    return catagories.map((categoryObj) => {
+        return [categoryObj.slug, categoryObj.description]
+    })
+}
+
+exports.sortUsers = (users) => {
+    return users.map((userObj) => {
+        return[userObj.username, userObj.name, userObj.avatar_url]
+    })
+}
