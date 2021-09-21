c65ea9b3ae7c461e020fb140609a8adc4023a436
Dan <danielkbeale@gmail.com>
2021-09-21 17:08:21 +0100

    controllers added

---
+++ controllers/category-controllers.js
@@ -0,0 +1,15 @@
+const { fetchCategories } = require("../models/category-models");
+
+exports.getCategories = (req, res, next) => {
+  fetchCategories()
+    .then((categories) => {
+      res.status(200).send({ categories: categories });
+    })
+    .then((err) => {
+        next(err)
+    });
+};
+
+exports.getReviewById = (req, res, next) => {
+    fetchReview().then(() => {})
+}
