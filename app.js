d279a10760cf176083dc861ae5324e7ee993532b
Dan <danielkbeale@gmail.com>
2021-09-22 23:57:58 +0100

    app updated upto getReview

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const { getCategories, getReviewById } = require('./controllers/category-controllers')
-const { handle404Err } = require('./errors/errors')
+const { getCategories, getReviewById, patchReviewVoteById, getReviews } = require('./controllers/controllers')
 const app = express();
+const { handle404Err, handle400Err } = require ("./errors/errors")
+
+app.use(express.json());
 
 app.get('/api/categories', getCategories);
 app.get('/api/reviews/:review_id', getReviewById)
+app.patch('/api/reviews/:review_id', patchReviewVoteById)
 
-app.all('*', (req, res) => {
+app.get('/api/reviews', getReviews)
+
+app.all('/*', (req, res) => {
     res.status(404).send({ msg: "Not Found!" })
+})
+
+app.use(handle404Err);
+app.use(handle400Err);
+app.use((err, req, res, next) => {
+    console.log(err, "<<<<ERRRRRRROR");
 })
 
 module.exports = app
