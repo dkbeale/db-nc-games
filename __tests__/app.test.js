41f39929ec130b7e46b1d8260dbb46c07fb6a640
Dan <danielkbeale@gmail.com>
2021-09-22 23:54:47 +0100

    app test updated getReviews

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,6 @@ describe("GET /api/categories ", () => {
       .get("/api/categories")
       .expect(200)
       .then((res) => {
-        console.log(res.body.categories)
         expect(res.body.categories).toEqual(expect.any(Array));
         expect(res.body.categories).toHaveLength(4);
         expect(typeof res.body.categories[0]).toEqual("object");
@@ -28,12 +27,12 @@ describe("GET /api/categories ", () => {
       });
   });
 });
-// res.statusCode, res.statusMessage
 
-describe.only("GET /api/review/:review_id", () => {
-  test("200 returns review by id number", () => {
+
+describe("GET /api/review/:review_id", () => {
+  test("200: returns review by id number", () => {
     return request(app)
-      .get("/api/review/1")
+      .get("/api/reviews/1")
       .expect(200)
       .then((res) => {
           expect(res.body.review).toEqual({
@@ -45,9 +44,96 @@ describe.only("GET /api/review/:review_id", () => {
               'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
             review_body: 'Farmyard fun!',
             category: 'euro game',
-            created_at: new Date(1610964020514),
+            created_at: "2021-01-18T00:00:00.000Z",
             votes: 1
           })
       });
+  });
+  test('404: returns not found when searching for a review that doesnt exist', () => {
+    return request(app)
+      .get("/api/reviews/118")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Review Not Found!")
+      })
+  });
+  test('404: returns when misspelt review ', () => {
+    return request(app)
+      .get("/api/review/118")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Not Found!")
+      })
+  });
+});
+
+describe('PATCH /api/reviews/:review_id', () => {
+  test('201: patch updates vote count and returns updated review', () => {
+    return request(app)
+      .patch("/api/reviews/1")
+      .send({ inc_votes: 2})
+      .expect(201)
+      .then((res) => {
+        expect(res.body.review).toEqual({
+          review_id : 1,
+          title: 'Agricola',
+          designer: 'Uwe Rosenberg',
+          owner: 'mallionaire',
+          review_img_url:
+            'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+          review_body: 'Farmyard fun!',
+          category: 'euro game',
+          created_at: "2021-01-18T00:00:00.000Z",
+          votes: 3
+        })
+      })
+  });
+  test('404: returns when review_id does not exist', () => {
+    return request(app)
+      .patch("/api/reviews/118")
+      .send({ inc_votes: 2 })
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Review Not Found!")
+      })
+  });
+  test('400: returns when vote is not a number', () => {
+    return request(app)
+      .patch("/api/reviews/1")
+      .send({ inc_votes: "fish" })
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request")
+      })
+  });
+  test('404: returns when misspelt review ', () => {
+    return request(app)
+      .patch("/api/review/1")
+      .send({ inc_votes: 2 })
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Not Found!")
+      })
+  });
+});
+
+describe.only('GET /api/reviews', () => {
+  test('200: returns all reviews', () => {
+    return request(app)
+      .get('/api/reviews')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.reviews).toEqual(expect.any(Array));
+        expect(res.body.reviews).toHaveLength(13);
+        expect(typeof res.body.reviews[0]).toEqual("object");
+      })
+  });
+  test('404: not found, incorrectly spelt endpoint etc', () => {
+    return request(app)
+      .get('/api/reeviews')
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Not Found!")
+      })
   });
 });
