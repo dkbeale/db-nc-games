c0134edef9b7ef76c3033e35d2a21d337bfc773e
Dan <danielkbeale@gmail.com>
2021-09-22 23:57:31 +0100

    errors updated

--- errors/errors.js
+++ errors/errors.js
@@ -1,3 +1,15 @@
 exports.handle404Err = ((err, req, res, next) => {
-    
+    if (err.status === 404) {
+        res.status(404).send({ msg: err.msg })
+    } else {
+        next(err);
+    }
+})
+
+exports.handle400Err = ((err, req, res, next) => {
+    if (err.status === 400) {
+        res.status(400).send({ msg: err.msg })
+    } else {
+        next(err)
+    }
 })
