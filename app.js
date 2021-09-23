768c4fa302f4aff725c32b934819bfe69a310027
Dan <danielkbeale@gmail.com>
2021-09-24 00:23:27 +0100

    app updated

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const { getCategories, getReviewById, patchReviewVoteById, getReviews } = require('./controllers/controllers')
+const express = require("express");
+const {
+  getCategories,
+  getReviewById,
+  patchReviewVoteById,
+  getReviews,
+  getCommentsByReview,
+  postCommentById
+} = require("./controllers/controllers");
 const app = express();
-const { handle404Err, handle400Err } = require ("./errors/errors")
+const { handle404Err, handle400Err } = require("./errors/errors");
 
 app.use(express.json());
 
-app.get('/api/categories', getCategories);
-app.get('/api/reviews/:review_id', getReviewById)
-app.patch('/api/reviews/:review_id', patchReviewVoteById)
+app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReviewById);
+app.patch("/api/reviews/:review_id", patchReviewVoteById);
 
-app.get('/api/reviews', getReviews)
+app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id/comments", getCommentsByReview);
+app.post("/api/reviews/:review_id/comments", postCommentById)
 
-app.all('/*', (req, res) => {
-    res.status(404).send({ msg: "Not Found!" })
-})
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Not Found!" });
+});
 
 app.use(handle404Err);
 app.use(handle400Err);
-app.use((err, req, res, next) => {
-    console.log(err, "<<<<ERRRRRRROR");
-})
+// app.use((err, req, res, next) => {
+//   console.log(err, "<<<<ERRRRRRROR");
+// });
 
-module.exports = app
+module.exports = app;
