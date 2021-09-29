63537c867b25808c4733018d12c0fce66028a45b
Dan <danielkbeale@gmail.com>
2021-09-29 23:11:08 +0100

    all completed except get reviews

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,23 +31,25 @@ describe("GET /api/categories ", () => {
 });
 
 
-describe("GET /api/review/:review_id", () => {
+describe("GET /api/reviews/:review_id", () => {
   test("200: returns review by id number", () => {
     return request(app)
-      .get("/api/reviews/1")
+      .get("/api/reviews/2")
       .expect(200)
       .then((res) => {
           expect(res.body.review).toEqual({
-            review_id : 1,
-            title: 'Agricola',
-            designer: 'Uwe Rosenberg',
-            owner: 'mallionaire',
+            review_id : 2,
+            title: 'Jenga',
+            designer: 'Leslie Scott',
+            owner: 'philippaclaire9',
             review_img_url:
               'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
-            review_body: 'Farmyard fun!',
-            category: 'euro game',
+            review_body: 'Fiddly fun for all the family',
+            category: 'dexterity',
             created_at: "2021-01-18T00:00:00.000Z",
-            votes: 1
+            votes: 5,
+            comment_count: 3
+
           })
       });
   });
@@ -65,6 +67,14 @@ describe("GET /api/review/:review_id", () => {
       .expect(404)
       .then((res) => {
         expect(res.body.msg).toBe("Not Found!")
+      })
+  });
+  test('400: returns when invalid ID', () => {
+    return request(app)
+      .get('/api/reviews/dog')
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe('Bad Request - Not an ID')
       })
   });
 });
@@ -105,7 +115,7 @@ describe('PATCH /api/reviews/:review_id', () => {
       .send({ inc_votes: "fish" })
       .expect(400)
       .then((res) => {
-        expect(res.body.msg).toBe("Bad Request")
+        expect(res.body.msg).toBe("Invalid ID or Vote")
       })
   });
   test('404: returns when misspelt endpoint ', () => {
@@ -117,10 +127,19 @@ describe('PATCH /api/reviews/:review_id', () => {
         expect(res.body.msg).toBe("Not Found!")
       })
   });
+  test('400: Invalid ID', () => {
+    return request(app)
+      .patch('/api/reviews/fish')
+      .send({ inc_votes: 2 })
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe('Invalid ID or Vote')
+      })
+  });
 });
 
 describe('GET /api/reviews', () => {
-  test('200: returns all reviews', () => {
+  test('200: returns all reviews when passed no queries', () => {
     return request(app)
       .get('/api/reviews')
       .expect(200)
@@ -128,6 +147,16 @@ describe('GET /api/reviews', () => {
         expect(res.body.reviews).toEqual(expect.any(Array));
         expect(res.body.reviews).toHaveLength(13);
         expect(typeof res.body.reviews[0]).toEqual("object");
+        expect(res.body.reviews[6]).toMatchObject({
+          owner: expect.any(String),
+          title: expect.any(String),
+          review_id: expect.any(Number),
+          category: expect.any(String),
+          review_img_url: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          comment_count: expect.any(String)
+        })
       })
   });
   test('404: not found, incorrectly spelt endpoint etc', () => {
@@ -136,6 +165,41 @@ describe('GET /api/reviews', () => {
       .expect(404)
       .then((res) => {
         expect(res.body.msg).toBe("Not Found!")
+      })
+  });
+  test('200: default returns list of reviews in DESC order by date', () => {
+    return request(app)
+      .get('/api/reviews')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.reviews).toBeSortedBy('created_at', { descending: true, coerce: true});
+      })
+  });
+  test('200: sorts DESC by chosen column value', () => {
+    return request(app)
+      .get('/api/reviews?sort_by=owner')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.reviews).toBeSortedBy('owner', { descending: true })
+      })
+  });
+  test('200: sorts in chosen order', () => {
+    return request(app)
+      .get('/api/reviews?sort_by=owner&order=asc')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.reviews).toBeSortedBy('owner')
+      })
+  });
+  test.skip('200: sorts by category value', () => {
+    return request(app)
+      .get('/api/reviews?category=owner')
+      .expect(200)
+      .then((res) => {
+        console.log(res.body.reviews[0])
+        expect(res.body.reviews[0]).toEqual({
+          owner: expect.any(String)
+        })
       })
   });
 });
@@ -196,23 +260,57 @@ describe('POST /api/reviews/:review_id/comments', () => {
         })
       })
   });
-  test('400: bad request, trying to post when review_id doesnt exist', () => {
+  test('404: bad request, trying to post when review_id doesnt exist', () => {
     return request(app)
       .post('/api/reviews/14/comments')
       .send({ username: "philippaclaire9", body: "I love this game!"})
-      .expect(400)
+      .expect(404)
       .then((res) => {
-        expect(res.body.msg).toBe("Bad Request, Review Does Not Exist")
+        expect(res.body.msg).toBe("Review Not Found")
       })
   });
-  test('400: bad request, user does not exist', () => {
+  test('404: bad request, username does not exist', () => {
     return request(app)
       .post('/api/reviews/1/comments')
       .send({ username: "dan", body: "I love this game!"})
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("User Not Found")
+      })
+  });
+  test('400: missing required fields', () => {
+    return request(app)
+      .post('/api/reviews/1/comments')
+      .send({})
       .expect(400)
       .then((res) => {
-        expect(res.body.msg).toBe("Bad Request, User Does Not Exist")
+        expect(res.body.msg).toBe('Missing required field')
       })
+  });
+  test('400: Invalid reviewID', () => {
+    return request(app)
+    .post('/api/reviews/apple/comments')
+    .send({ username: "philippaclaire9", body: "I love this game!" })
+    .expect(400)
+    .then((res) => {
+      expect(res.body.msg).toBe('Invalid ID')
+    })
+  });
+  test('201: creates comment, ignoring extra properties', () => {
+    return request(app)
+    .post('/api/reviews/1/comments')
+    .send({ username: "philippaclaire9", face: "Hullo There", body: "I love this game!"})
+    .expect(201)
+    .then((res) => {
+      expect(res.body.comment).toMatchObject({
+        comment_id: expect.any(Number),
+        author: expect.any(String),
+        review_id: expect.any(Number),
+        votes: expect.any(Number),
+        created_at: expect.any(String),
+        body: expect.any(String)
+      })
+    })
   });
 });
 
