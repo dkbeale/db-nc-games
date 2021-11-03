88b7d45fe7e48b50fb83d9bc9282912cdd6af432
Dan <danielkbeale@gmail.com>
2021-11-03 00:29:55 +0000

    postCategoy added

--- controllers/categories-controllers.js
+++ controllers/categories-controllers.js
@@ -1,4 +1,4 @@
-const { fetchCategories } = require('../models/categories-models')
+const { fetchCategories, createCategory } = require('../models/categories-models')
 
 exports.getCategories = (req, res, next) => {
     fetchCategories()
@@ -8,4 +8,14 @@ exports.getCategories = (req, res, next) => {
       .then((err) => {
         next(err);
       });
-  };
+};
+
+exports.postCategory = (req, res, next) => {
+  const { slug: slug, description: description } = req.body;
+  createCategory(slug, description)
+    .then((category) => {
+      res.status(201).send({ category: category })
+    }).catch((err) => {
+      next(err);
+    })
+}
--- models/categories-models.js
+++ models/categories-models.js
@@ -1,7 +1,42 @@
 const db = require("../db/connection");
 
 exports.fetchCategories = () => {
-    return db.query(`SELECT * FROM categories`).then((categories) => {
-      return categories.rows;
+  return db.query(`SELECT * FROM categories`).then((categories) => {
+    return categories.rows;
+  });
+};
+
+exports.createCategory = (slug, description) => {
+  if (!slug || !description)
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request: Missing required field",
     });
-  };
+  if (typeof slug !== "string")
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request: Slug cannot be a number",
+    });
+
+  return db
+    .query(`SELECT * FROM categories WHERE slug = $1`, [slug])
+    .then((resTest) => {
+      if (resTest.rows.length === 0) {
+        return db
+          .query(
+            `INSERT INTO categories (slug, description) VALUES ($1, $2) RETURNING *`,
+            [slug, description]
+          )
+          .then((res) => {
+            return res.rows[0];
+          });
+      } else {
+        return Promise.reject({
+          status: 400,
+          msg: "Bad Request: Category already exists",
+        });
+      }
+    });
+};
+
+
