4c6a7b8c28527405ac07f750c1f0f81b27f1d547
Dan <danielkbeale@gmail.com>
2021-11-05 00:11:15 +0000

    added POST review

--- routers/reviews.router.js
+++ routers/reviews.router.js
@@ -1,14 +1,22 @@
-const { getReviewById, patchReviewVoteById, getReviews } = require('../controllers/reviews.controllers')
+const { getReviewById, patchReviewVoteById, getReviews, postReview } = require('../controllers/reviews.controllers')
 const { getCommentsByReview, postCommentById } = require('../controllers/comments.controllers')
 
 const reviewsRouter = require('express').Router();
 
 
-reviewsRouter.route('/').get(getReviews)
-reviewsRouter.route('/:review_id/comments').get(getCommentsByReview).post(postCommentById)
+reviewsRouter
+        .route('/')
+        .get(getReviews)
+        .post(postReview);
+
+reviewsRouter
+        .route('/:review_id/comments')
+        .get(getCommentsByReview)
+        .post(postCommentById);
+
 reviewsRouter
         .route('/:review_id')
         .get(getReviewById)
-        .patch(patchReviewVoteById)
+        .patch(patchReviewVoteById);
 
 module.exports = reviewsRouter;
