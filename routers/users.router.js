e7a640b4c9d05443d3586c1fa2301f23c21d593f
Dan <danielkbeale@gmail.com>
2021-11-05 00:12:02 +0000

    added GET reviews by user

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,8 +1,18 @@
 const { getUsers, getUserByUsername, postUser } = require('../controllers/users.controllers')
-
+const { getReviewsByUser } = require('../controllers/reviews.controllers')
 const usersRouter = require('express').Router();
 
-usersRouter.route('/').get(getUsers).post(postUser)
-usersRouter.route('/:username').get(getUserByUsername)
+usersRouter
+        .route('/')
+        .get(getUsers)
+        .post(postUser);
+
+usersRouter
+        .route('/:username')
+        .get(getUserByUsername);
+
+usersRouter
+        .route('/:username/reviews')
+        .get(getReviewsByUser);
 
 module.exports = usersRouter;
